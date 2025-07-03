7378983d1cd02ac6bb22849594dbd32d30230fee
Carmen Watkins <cebel2@uoregon.edu>
2025-07-03 08:08:21 -0700

    comment out file save

--- allometry/merge_allometric_relationships.R
+++ allometry/merge_allometric_relationships.R
@@ -34,13 +34,14 @@ source(paste0(allo_path, "twil_allometry_testing.R"))
 ## merge all together
 allo.df <- do.call("rbind", list(ACAM.allo.output, ANAR.allo.output, AMME.allo.output, BRHO.allo.output, BRNI.allo.output, CESO.allo.output, CLPU.allo.output, GITR.allo.output, LENI.allo.output, LOMU.allo.output, MAEL.allo.output, MICA.allo.output, PLER.allo.output, PLNO.allo.output, TACA.allo.output, THIR.allo.output, TWIL.allo.output))
 
+## create a version to save & share
 allo.df.save = allo.df %>%
   rownames_to_column() %>%
   mutate(allo_type = ifelse(grepl("total",rowname), "total.biomass.g", rowname),
          allo_type = ifelse(allo_type %in% c("1", "11", "12"), NA, allo_type)) %>%
   select(-rowname)
 
-write.csv(allo.df.save, "data/allometry_model_parameters_20250703.csv")
+## write.csv(allo.df.save, "data/allometry_model_parameters_20250703.csv")
 
 ## clean up env
 rm(list = c("ACAM.allo.output", "ANAR.allo.output", "AMME.allo.output", "BRHO.allo.output", "BRHO.tb.allo.output", "BRNI.allo.output", "CESO.allo.output", "CLPU.allo.output", "GITR.allo.output", "LENI.allo.output", "LOMU.allo.output", "MAEL.allo.output", "MICA.allo.output", "PLER.allo.output", "PLER.allo.output.CAREER", "PLNO.allo.output", "TACA.allo.output", "THIR.allo.output", "TWIL.allo.output", allo.df.save))
